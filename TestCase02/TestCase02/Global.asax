f69d7796ba70620c63911e04e0ec663a8c5d8cda
michellehsieh <michelle.hsieh035@gmail.com>
2017-05-08 12:01:07 +0800

    新專案Release

---
+++ TestCase02/TestCase02/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestCase02.MvcApplication" Language="C#" %>
---
+++ TestCase02/TestCase02/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestCase02.Startup))]
+namespace TestCase02
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
