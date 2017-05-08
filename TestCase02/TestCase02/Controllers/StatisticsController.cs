dd8b24d3a8c0b1d05415024e02c189e747f2ca77
michellehsieh <michelle.hsieh035@gmail.com>
2017-05-08 17:54:40 +0800

    實作一個清單頁面，顯示欄位有「客戶名稱、聯絡人數量、銀行帳戶數量」共三個欄位，此功能要在資料庫中建立一個檢視表，並且匯入 EDMX

---
+++ TestCase02/TestCase02/Controllers/StatisticsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestCase02.Models;
+
+namespace TestCase02.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        // GET: Statistics
+        public ActionResult Index()
+        {
+            客戶資料Entities1 db = new 客戶資料Entities1();
+
+            var data = db.客戶資料統計表.AsQueryable();
+
+            return View(data.ToList());
+        }
+    }
+}
